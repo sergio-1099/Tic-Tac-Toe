db7d89322403c1f0b75f2cc8fe0e9595479727f2
Sergio Guerra <sergioguerra6188@gmail.com>
2023-02-11 14:25:00 -0500

    Add printBoard method and edit @spaces
    This method will traverse through the @spaces array and print the
    elements along with separators to resemble a tic tac toe board.
    
    Also edited the default value of the @spaces instance variable to a " "
    (space) so that it would not grow in size when it gets filled with a
    symbol.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,29 @@
+require 'pry-byebug'
+
 class Board
   def initialize
-    @spaces = Array.new(3) { Array.new(3, nil) }
+    @spaces = Array.new(3) { Array.new(3, " ") }
   end
   
   def fillSpace(symbol, x_coordinate, y_coordinate)
     @spaces[x_coordinate][y_coordinate] = symbol
+  end
+
+  def printBoard
+    @spaces.each_index do |rows|
+      @spaces[rows].each_index do |columns|
+        if (columns == 2)
+          print "\s\s\s#{@spaces[rows][columns].to_s}\s\s\s"
+        else
+          print "\s\s\s#{@spaces[rows][columns].to_s}\s\s\s|"
+        end
+      end
+      if (rows == 2)
+        puts ""
+      else
+        puts ""
+        puts "-----------------------"
+      end
+    end
   end
 end
