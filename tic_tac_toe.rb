f6e9dc18c4cd6839e70c7d39e71fe27b3b6fcd02
Sergio Guerra <sergioguerra6188@gmail.com>
2023-02-11 13:35:43 -0500

    Add fillSpace method that will fill board position
    Will be called by program to fill in space position by coordinates.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,4 +2,8 @@ class Board
   def initialize
     @spaces = Array.new(3) { Array.new(3, nil) }
   end
+  
+  def fillSpace(symbol, x_coordinate, y_coordinate)
+    @spaces[x_coordinate][y_coordinate] = symbol
+  end
 end
